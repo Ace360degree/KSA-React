a5abfd8097fc20046c73cfcb48ded58daa0a4996
Hammed <customercare@a360pl.com>
2024-09-14 17:40:20 +0530

    About and Contact Client Component

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    "extends": "next",
-    "rules": {
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-page-custom-font": "off"
-    },
     images: {
       remotePatterns: [
         {
