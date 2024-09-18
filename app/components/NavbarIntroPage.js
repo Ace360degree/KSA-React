e36dfbd21bb6342f4ad364cebd8563b97f7d5f88
Hammed <customercare@a360pl.com>
2024-09-18 13:33:54 +0530

    Final Changes v2

--- app/components/NavbarIntroPage.js
+++ app/components/NavbarIntroPage.js
@@ -1,6 +1,6 @@
 'use client';
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function NavbarIntroPage({heading,subheading}){
@@ -8,9 +8,11 @@ export default function NavbarIntroPage({heading,subheading}){
     const [subHeading,setSubHead]= useState(heading);
     const [smallHeading,setsmallHeading]= useState(subheading);
 
+
+
     return(
         <>
-            <div id="navbar" className="active">
+            <div id="navbar" className="active" >
                 <div className="navbar-control">
                     <div className="nav-logo">
                         <Link href={'/'}><div className="logo-main">KSA<span>{subHeading}</span></div></Link>
