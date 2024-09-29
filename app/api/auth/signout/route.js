0f62c91dc158d850a697bc87a725892965e2e114
Shaikh Hameed <hamidkshaikh@gmail.com>
2024-09-29 22:39:32 +0530

    Api change - Logout

--- app/api/auth/signout/route.js
+++ app/api/auth/signout/route.js
@@ -8,7 +8,8 @@ export async function POST(req) {
   // const session = await getServerSession(authOptions);
 
     // Perform any additional logout logic here if needed (e.g., log events, revoke tokens, etc.)
-    cookies().set('isLogin',false);
+    cookies().set('isLogin', '', { expires: new Date(0) });
+    cookies().set('authToken', '', { expires: new Date(0) });
     return NextResponse.json({ message: 'Logged out successfully' });
 
   // return NextResponse.json({ error: 'No active session' }, { status: 400 });
