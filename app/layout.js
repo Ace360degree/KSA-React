cb73c3585648d7d269a61c41418f77f8ceb4a7ce
Hammed <customercare@a360pl.com>
2024-09-30 15:21:13 +0530

    New Favicon Added

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head> 
       <body className="">
         <AuthProvider>
