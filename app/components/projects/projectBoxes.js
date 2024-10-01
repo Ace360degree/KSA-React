31028603eafbde7ec364eb1169a0d3f9041fcc70
Hammed <customercare@a360pl.com>
2024-10-01 11:21:27 +0530

    Cache True

--- app/components/projects/projectBoxes.js
+++ app/components/projects/projectBoxes.js
@@ -57,7 +57,7 @@ export default function ProjectBoxes() {
   useEffect(()=>{
     async function getProjectsAPI(){
 
-      const fetchProjects = await fetch('/api/get-projects',{method:'GET',headers: {'Content-Type': 'application/json',}});
+      const fetchProjects = await fetch('/api/get-projects',{method:'GET',headers: {'Content-Type': 'application/json',}},{ cache: 'no-store' });
       const getProjects = await (fetchProjects.json());
       
       setProjects(getProjects.projects);
