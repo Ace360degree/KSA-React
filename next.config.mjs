72db25d97c65a2bf3e14fe5af2974b9c9b34c8cf
Hammed <customercare@a360pl.com>
2024-10-01 12:22:55 +0530

    Next config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https', // Assuming your image uses HTTPS
-          hostname: '360clients.in',
-          port: '', // No port specified in the error message
-        },
-      ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '360clients.in',
+        port: '',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        // Apply no-cache headers to all image files
+        source: '/:path*{png,jpg,jpeg,gif,webp}', // Adjust as needed for your image formats
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+          {
+            key: 'Surrogate-Control',
+            value: 'no-store',
+          },
+        ],
+      },
+    ];
+  },
+};
 
-  };
-  
-  export default nextConfig;
+export default nextConfig;
