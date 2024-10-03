8b79c47ef9c836ebf48e5dd2c03ac0d4e74d63cf
Hammed <customercare@a360pl.com>
2024-10-03 19:38:04 +0530

    KSA Final Chnages v2-245

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,7 @@
 
 'use client';
 import dynamic from 'next/dynamic';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 const AboutComponent = dynamic(() => import('./aboutComponent'), {
     ssr: false,
   }) 
