373ac5f85f41ccfce917d550e3ac82f16b260a12
Hammed <customercare@a360pl.com>
2024-10-11 12:33:01 +0530

    Ideas Page Visible 2

--- app/ideas/ideasComponent.js
+++ app/ideas/ideasComponent.js
@@ -1,3 +1,4 @@
+'use client';
 import { useEffect,useState } from "react";
 import LightTheme from "../components/body/lightTheme";
 import NavbarIntroPage from "../components/NavbarIntroPage";
--- app/ideas/page.js
+++ app/ideas/page.js
@@ -1,7 +1,7 @@
 'use client';
 import dynamic from "next/dynamic";
 
-const IdeasComponent = dynamic(() => import("./ideasComponent"));
+const IdeasComponent = dynamic(() => import("./ideasComponent"),{ssr: false,});
 
 
 export default  function Ideas(){
