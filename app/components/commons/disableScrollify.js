1e7faec43a2114b4d7769885a5d7aea9119d1d2a
Hammed <customercare@a360pl.com>
2024-10-28 13:01:47 +0530

    Ksa V2.0.28.04

--- app/components/commons/disableScrollify.js
+++ app/components/commons/disableScrollify.js
@@ -5,16 +5,21 @@ import 'jquery-scrollify';
 
 
 export default function ScrollifyDisabled(){
-    useEffect(()=>{
-        $(document).ready(function(){
-
+    useEffect(() => {
+        // Ensure scrollify only initializes if it's available
+        if ($.scrollify) {
+            // Reset scroll to top and destroy scrollify on mount
             $.scrollify.move(0);
             $.scrollify.destroy();
-    
-        });
-        
-       
-      },[])
+
+            // Clean up by destroying scrollify if needed when component unmounts
+            return () => {
+                if ($.scrollify) {
+                    $.scrollify.destroy();
+                }
+            };
+        }
+    }, []);
 
     return;
 }
