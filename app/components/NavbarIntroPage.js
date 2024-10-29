2f6a3f35aa4e4d5df6e568b2af5a0c5dc7eac4b0
Hammed <customercare@a360pl.com>
2024-10-29 19:05:32 +0530

    Ksa V2.0.29.08

--- app/components/NavbarIntroPage.js
+++ app/components/NavbarIntroPage.js
@@ -18,19 +18,24 @@ export default function NavbarIntroPage({heading,subheading}){
 
     },[]);
 
+    
+    function preventScroll(event) {
+        event.preventDefault();
+      }
+
 
     return(
         <>
             <div id="navbar" className={activeClass?'active':''} >
                 <div className="navbar-control">
                     <div className="nav-logo">
-                        <Link href={'/'} scroll={false}><div className="logo-main">KSA<span>{subHeading}</span></div></Link>
+                        <Link href={'/'} onMouseEnter={preventScroll} onTouchStart={preventScroll}><div className="logo-main">KSA<span>{subHeading}</span></div></Link>
                         {smallHeading!=''? 
-                            <Link href={`/expertise?category=${smallHeading}`} scroll={false}><div className="header-content signifier text-capitalize">{smallHeading}</div></Link>
+                            <Link href={`/expertise?category=${smallHeading}`} onMouseEnter={preventScroll} onTouchStart={preventScroll}><div className="header-content signifier text-capitalize">{smallHeading}</div></Link>
                         : ''}
                     </div>
                     <div className="nav-img-logo">
-                        <Link href={'/home'} scroll={false}><img className="ksa-logo" width="40" src="/images/ksa-logo.png"/></Link>
+                        <Link href={'/home'} onMouseEnter={preventScroll} onTouchStart={preventScroll}><img className="ksa-logo" width="40" src="/images/ksa-logo.png"/></Link>
                     </div>
                 </div>
             </div>
