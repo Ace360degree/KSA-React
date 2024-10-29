6829d083e4e529e4213706cada67af38a7b2ff6e
Hammed <customercare@a360pl.com>
2024-10-29 19:27:49 +0530

    Ksa V2.0.29.14

--- app/components/NavbarIntroPage.js
+++ app/components/NavbarIntroPage.js
@@ -31,13 +31,13 @@ export default function NavbarIntroPage({heading,subheading}){
             <div id="navbar" className={activeClass?'active':''} >
                 <div className="navbar-control">
                     <div className="nav-logo">
-                        <span onClick={()=>{routetoPage('/')}} ><div className="logo-main">KSA<span>{subHeading}</span></div></span>
+                        <span className="cursor-pointer" onClick={()=>{routetoPage('/')}} ><div className="logo-main">KSA<span>{subHeading}</span></div></span>
                         {smallHeading!=''? 
-                            <span onClick={()=>{routetoPage(`/expertise?category=${smallHeading}`)}} ><div className="header-content signifier text-capitalize">{smallHeading}</div></span>
+                            <span className="cursor-pointer" onClick={()=>{routetoPage(`/expertise?category=${smallHeading}`)}} ><div className="header-content signifier text-capitalize">{smallHeading}</div></span>
                         : ''}
                     </div>
                     <div className="nav-img-logo">
-                        <span onClick={()=>{routetoPage('/home')}}><img className="ksa-logo" width="40" src="/images/ksa-logo.png"/></span>
+                        <span className="cursor-pointer" onClick={()=>{routetoPage('/home')}}><img className="ksa-logo" width="40" src="/images/ksa-logo.png"/></span>
                     </div>
                 </div>
             </div>
