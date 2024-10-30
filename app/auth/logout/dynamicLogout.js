6a1c3c430de6a70742c44c43ccd46845254ce7a3
Hammed <customercare@a360pl.com>
2024-10-30 17:27:15 +0530

    Ksa V2.0.30.4

--- app/auth/logout/dynamicLogout.js
+++ app/auth/logout/dynamicLogout.js
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import { useAuth } from '@/app/context/AuthContext';
 import { useRouter, useSearchParams } from 'next/navigation';
+import DarkTheme from '@/app/components/body/darkTheme';
 
 export default function DynamicLogout() {
   const [loading, setLoading] = useState(true);
@@ -10,7 +11,10 @@ export default function DynamicLogout() {
   const searchParams = useSearchParams(); 
   const paramsPage = searchParams?.get('route');
   const { logout } = useAuth();
-  useEffect(() => {
+
+  const [showError,setShowError] = useState(false)
+
+
     const handleLogout = async () => {
       setLoading(true); // Set loading at the start of the logout process
       
@@ -31,16 +35,32 @@ export default function DynamicLogout() {
       }
     };
 
-    handleLogout();
-  }, [paramsPage, router]);
+    
+
 
   return (
+    <>
+    <DarkTheme/>
     <div className="logout-window">
-      {loading ? (
-        <div>Logging out.<br />Please wait...</div>
-      ) : (
-        <div>Logout failed. Please try again.</div>
-      )}
+
+      <div className="contact-alert-box">
+          <div className="contact-alert">
+              <h1>Are you sure, You want to log out?</h1>
+              <p></p>
+              <div className='d-flex w-100 justify-content-end' style={{gap:'8px'}}>
+                <button type="button" className='btn btn-outline-dark text-white' onClick={()=>{router.back()}}>Back</button>
+                <button className="btn btn-light px-4" onClick={()=>{handleLogout()}}>Logout</button>
+              </div>
+              <div className='w-100 text-center'>
+                {showError?
+                <p className='text-danger'>Something went Wrong. Please try again.</p>
+                :''}
+              </div>
+          </div>
+      </div>
+
+
     </div>
+    </>
   );
 }
