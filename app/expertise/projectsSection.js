97200a0888dd1af8274ce25706de490009cbfff4
Hammed <customercare@a360pl.com>
2024-11-11 10:34:25 +0530

    Ksa V2.0.11.11.1

--- app/expertise/projectsSection.js
+++ app/expertise/projectsSection.js
@@ -174,7 +174,7 @@ export default function ProjectSection({ section, slides, essecials, points, vid
   }
   else if (section.section_type === 5) {
     return (
-      <div ref={InfoSection} className='remove-transparent project-underline-section project-info-section' style={{ transition: 'all 1.2s ease' }}>
+      <div ref={InfoSection} className='remove-transparent project-underline-section project-info-section project-essentials' style={{ transition: 'all 1.2s ease' }}>
         <ProjectEssencials essecials={essecials} points={points} />
       </div>
     );
