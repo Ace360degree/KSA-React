506af182916279b160138ece777bc54dd1eb266b
Hammed <customercare@a360pl.com>
2024-11-25 19:51:51 +0530

    Video Player Fixed 3

--- app/expertise/projectsSection.js
+++ app/expertise/projectsSection.js
@@ -74,98 +74,98 @@ export default function ProjectSection({ section, slides, essecials, points, vid
 
 
 
-//   if (section.section_type === 1) {
-//     return (
-//       <>
-//         <div className="project-info-section remove-transparent project-border-bottom project-info-flex signifier" ref={InfoSection}>
-//           <div className='info-block-content'>
-//                <h2 className='info-block-title text-start'>{section.section_title}</h2>
-//                 <h4 className='info-block-desc text-start'>{section.content}</h4>
-//           </div>
-//           <div className='info-block-image'>
-//                {windowSize.width <= 840 && section.section_image_mobile ? 
-//                  <Image className='info-block-img' height={500} width={500} placeholder='blur' blurDataURL='/images/white-blur.png' src={process.env.NEXT_PUBLIC_SITE_URL + section.section_image_mobile} alt="KSA"/>
-//                  : windowSize.width > 840 && section.section_image ? 
-//                  <Image className='info-block-img' height={500} width={500} unoptimized placeholder='blur' blurDataURL='/images/white-blur.png' src={process.env.NEXT_PUBLIC_SITE_URL + section.section_image} alt="KSA"/> 
-//                  : null }
-//           </div>
-//         </div>
-//       </>
-//     );
-//   } else if (section.section_type === 2) {
-//     return (
-//         <div className="project-info-section remove-transparent project-border-bottom" ref={InfoSection}>
-//           <div className="row m-0 g-0">
-//             <div className=" col-lg-7">
-//               <div className="project-image-info">
-//                 <h2 className='text-start'>{section.section_title}</h2>
-//                 <h4 className='text-start'>{section.content}</h4>
-//               </div>
-//             </div>
-//             <div className=" col-lg-5">
-//               <div className="project-info-image info-slider-section">
-//                 <Splide options={SliderSettings}>
-//                   {slides.slides.map((slide, index) => (
-//                     <SplideSlide key={index}>
-//                       {windowSize.width <= 750 && slide.mobile ? 
-//                         <Image height={500} width={500} style={{ width: '100%', height: '100%' }} placeholder='blur' blurDataURL='/images/white-blur.png' src={process.env.NEXT_PUBLIC_SITE_URL + slide.mobile} alt="KSA" />
-//                         : windowSize.width > 750 && slide.desktop ?
-//                         <Image height={500} width={500} unoptimized style={{ width: '100%', height: '100%' }} placeholder='blur' blurDataURL='/images/white-blur.png' src={process.env.NEXT_PUBLIC_SITE_URL + slide.desktop} alt="KSA" />
-//                         : null}
-//                     </SplideSlide>
-//                   ))}
-//                 </Splide>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//     );
-//   } else if (section.section_type === 3) {
-//     return (
-//       <>
-//         {windowSize.width <= 750 && section.section_image_mobile ? 
-//           (
-//             <div className="img-border-section position-relative hero-image" style={{ minHeight: '100vh' }} ref={InfoSection}>
-//               <Image className='hero-image' height={500} width={500} style={{ width: '100%', height: '100vh', objectFit: 'cover' }} unoptimized placeholder='blur' blurDataURL='/images/white-blur.png' src={process.env.NEXT_PUBLIC_SITE_URL + section.section_image_mobile} alt="KSA" />
-//             </div>
-//           )
-//           : windowSize.width > 750 && section.section_image ? 
-//           (
-//             <div className=" img-border-section position-relative hero-image" style={{ minHeight: '100vh' }} ref={InfoSection}>
-//               <Image className='hero-image' height={500} width={500} unoptimized style={{ width: '100%', height: '100vh', objectFit: 'cover' }} placeholder='blur' blurDataURL='/images/white-blur.png' src={process.env.NEXT_PUBLIC_SITE_URL + section.section_image} alt="KSA" />
-//             </div>
-//           )
-//           : null}
-//       </>
-//     );
-// }
+  if (section.section_type === 1) {
+    return (
+      <>
+        <div className="project-info-section remove-transparent project-border-bottom project-info-flex signifier" ref={InfoSection}>
+          <div className='info-block-content'>
+               <h2 className='info-block-title text-start'>{section.section_title}</h2>
+                <h4 className='info-block-desc text-start'>{section.content}</h4>
+          </div>
+          <div className='info-block-image'>
+               {windowSize.width <= 840 && section.section_image_mobile ? 
+                 <Image className='info-block-img' height={500} width={500} placeholder='blur' blurDataURL='/images/white-blur.png' src={process.env.NEXT_PUBLIC_SITE_URL + section.section_image_mobile} alt="KSA"/>
+                 : windowSize.width > 840 && section.section_image ? 
+                 <Image className='info-block-img' height={500} width={500} unoptimized placeholder='blur' blurDataURL='/images/white-blur.png' src={process.env.NEXT_PUBLIC_SITE_URL + section.section_image} alt="KSA"/> 
+                 : null }
+          </div>
+        </div>
+      </>
+    );
+  } else if (section.section_type === 2) {
+    return (
+        <div className="project-info-section remove-transparent project-border-bottom" ref={InfoSection}>
+          <div className="row m-0 g-0">
+            <div className=" col-lg-7">
+              <div className="project-image-info">
+                <h2 className='text-start'>{section.section_title}</h2>
+                <h4 className='text-start'>{section.content}</h4>
+              </div>
+            </div>
+            <div className=" col-lg-5">
+              <div className="project-info-image info-slider-section">
+                <Splide options={SliderSettings}>
+                  {slides.slides.map((slide, index) => (
+                    <SplideSlide key={index}>
+                      {windowSize.width <= 750 && slide.mobile ? 
+                        <Image height={500} width={500} style={{ width: '100%', height: '100%' }} placeholder='blur' blurDataURL='/images/white-blur.png' src={process.env.NEXT_PUBLIC_SITE_URL + slide.mobile} alt="KSA" />
+                        : windowSize.width > 750 && slide.desktop ?
+                        <Image height={500} width={500} unoptimized style={{ width: '100%', height: '100%' }} placeholder='blur' blurDataURL='/images/white-blur.png' src={process.env.NEXT_PUBLIC_SITE_URL + slide.desktop} alt="KSA" />
+                        : null}
+                    </SplideSlide>
+                  ))}
+                </Splide>
+              </div>
+            </div>
+          </div>
+        </div>
+    );
+  } else if (section.section_type === 3) {
+    return (
+      <>
+        {windowSize.width <= 750 && section.section_image_mobile ? 
+          (
+            <div className="img-border-section position-relative hero-image" style={{ minHeight: '100vh' }} ref={InfoSection}>
+              <Image className='hero-image' height={500} width={500} style={{ width: '100%', height: '100vh', objectFit: 'cover' }} unoptimized placeholder='blur' blurDataURL='/images/white-blur.png' src={process.env.NEXT_PUBLIC_SITE_URL + section.section_image_mobile} alt="KSA" />
+            </div>
+          )
+          : windowSize.width > 750 && section.section_image ? 
+          (
+            <div className=" img-border-section position-relative hero-image" style={{ minHeight: '100vh' }} ref={InfoSection}>
+              <Image className='hero-image' height={500} width={500} unoptimized style={{ width: '100%', height: '100vh', objectFit: 'cover' }} placeholder='blur' blurDataURL='/images/white-blur.png' src={process.env.NEXT_PUBLIC_SITE_URL + section.section_image} alt="KSA" />
+            </div>
+          )
+          : null}
+      </>
+    );
+}
 
-//   else if (section.section_type === 4) {
-//     return slides.slides.length !== 0 ? (
-//       <div className="project-info-section img-border-section full-bleed-container position-relative hero-image" ref={InfoSection}>
-//         <Splide options={SliderSettings}>
-//           {slides.slides.map((slide, index) => (
-//             <SplideSlide key={index}>
-//               {windowSize.width <= 750 && slide.mobile ? 
-//                 <Image className='hero-image' height={500} width={500} style={{ width: '100%', height: '100vh', objectFit: "cover" }} placeholder='blur' blurDataURL='/images/white-blur.png' src={process.env.NEXT_PUBLIC_SITE_URL + slide.mobile} alt="KSA"/>
-//                 : windowSize.width > 750 && slide.desktop ? 
-//                 <Image className='hero-image' height={500} width={500} unoptimized style={{ width: '100%', height: '100vh', objectFit: "cover" }} placeholder='blur' blurDataURL='/images/white-blur.png' src={process.env.NEXT_PUBLIC_SITE_URL + slide.desktop} alt="KSA"/>
-//                 : null}
-//             </SplideSlide>
-//           ))}
-//         </Splide>
-//       </div>
-//     ) : null;
-//   }
-//   else if (section.section_type === 5) {
-//     return (
-//       <div ref={InfoSection} className='remove-transparent project-underline-section project-info-section project-essentials' style={{ transition: 'all 1.2s ease' }}>
-//         <ProjectEssencials essecials={essecials} points={points} />
-//       </div>
-//     );
-//   }
+  else if (section.section_type === 4) {
+    return slides.slides.length !== 0 ? (
+      <div className="project-info-section img-border-section full-bleed-container position-relative hero-image" ref={InfoSection}>
+        <Splide options={SliderSettings}>
+          {slides.slides.map((slide, index) => (
+            <SplideSlide key={index}>
+              {windowSize.width <= 750 && slide.mobile ? 
+                <Image className='hero-image' height={500} width={500} style={{ width: '100%', height: '100vh', objectFit: "cover" }} placeholder='blur' blurDataURL='/images/white-blur.png' src={process.env.NEXT_PUBLIC_SITE_URL + slide.mobile} alt="KSA"/>
+                : windowSize.width > 750 && slide.desktop ? 
+                <Image className='hero-image' height={500} width={500} unoptimized style={{ width: '100%', height: '100vh', objectFit: "cover" }} placeholder='blur' blurDataURL='/images/white-blur.png' src={process.env.NEXT_PUBLIC_SITE_URL + slide.desktop} alt="KSA"/>
+                : null}
+            </SplideSlide>
+          ))}
+        </Splide>
+      </div>
+    ) : null;
+  }
+  else if (section.section_type === 5) {
+    return (
+      <div ref={InfoSection} className='remove-transparent project-underline-section project-info-section project-essentials' style={{ transition: 'all 1.2s ease' }}>
+        <ProjectEssencials essecials={essecials} points={points} />
+      </div>
+    );
+  }
 
-   if (section.section_type === 6) {
+   else if (section.section_type === 6) {
     return (
       <div className="project-info-section video-section hero-image">
         <Video width={'100%'} height={'auto'} controls={true} src={process.env.NEXT_PUBLIC_SITE_URL+ video.video} />
