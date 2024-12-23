7f6398ca8ab83810170573505c21306e2ec80682
Hammed <customercare@a360pl.com>
2024-12-23 13:54:01 +0530

    Expertise Auth- Screen

---
+++ app/api/mail/sendMail.js
@@ -0,0 +1,35 @@
+// app/api/mail/sendMail.js
+import nodemailer from 'nodemailer';
+
+export async function sendMail({ to, subject, text, html }) {
+  try {
+    // Configure the mail transporter
+    const transporter = nodemailer.createTransport({
+      host: 'admin.kuwalsanamarchitekts.com', // Replace with your SMTP server
+      port: 587, // Common SMTP port
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: 'mailer@admin.kuwalsanamarchitekts.com', // Replace with your email
+        pass: 'dDqV$s_q*tM!', // Replace with your email password
+      },
+    });
+
+    // Define the mail options
+    const mailOptions = {
+      from: '"Kuwal Sanam Architekts" mailer@admin.kuwalsanamarchitekts.com', // Replace with your sender email
+      to, 
+      subject, 
+      text, 
+      html, 
+    };
+
+    // Send the email
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent:', info.messageId);
+
+    return true;
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return false;
+  }
+}
