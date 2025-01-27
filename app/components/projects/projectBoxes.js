9b2231871b9cd29f14367779255cc2758951cdc3
Hammed <customercare@a360pl.com>
2025-01-27 16:25:08 +0530

    Session chnages 8

--- app/components/projects/projectBoxes.js
+++ app/components/projects/projectBoxes.js
@@ -59,7 +59,7 @@ export default function ProjectBoxes() {
 
   const ReRouteIt =()=>{
     if(!session && path ==='/expertise'){
-      // router.push(`/auth/login?route=${path}`);
+      router.push(`/auth/login?route=${path}`);
     }
   }
   
