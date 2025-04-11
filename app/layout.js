4f0ccbe515998a36c7402ab88b8d26701d0ec19b
Hammed <customercare@a360pl.com>
2025-04-11 16:32:58 +0530

    Disclaimer Popup disable

--- app/layout.js
+++ app/layout.js
@@ -67,7 +67,7 @@ export default function RootLayout({ children }) {
         <Reduxprovider>
           <SessionWrapper>
           <CursorAudio/>
-          <CheckCookie/>
+          {/* <CheckCookie/> */}
           <HomeTextProvider>
           <CheckUserStatus/>  
           <UserActivity/>  
