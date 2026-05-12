9e37f7baeb234c27a25d9b69f11ba58b28e918e0
Ace360Degree-CC <166904430+Ace360Degree-CC@users.noreply.github.com>
2026-05-12 20:48:52 +0530

    Update db.js

--- app/api/db.js
+++ app/api/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 
 // Create a connection pool
 export const pool = mysql.createPool({
-  host: '66.116.237.145',
-  user: 'kuwalsan_kuwalsanam',
-  password: `KSA@admin1`,
-  database: 'kuwalsan_maindb',
+  host: '65.254.81.135',
+  user: 'kuwalsanamarchit_kuwal_mainuser',
+  password: `CZrK^OtQ$33^`,
+  database: 'kuwalsanamarchit_maindb',
   connectionLimit: 100,  // A more reasonable connection limit
   multipleStatements: true,  
   waitForConnections: true,
