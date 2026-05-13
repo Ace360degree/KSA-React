d9eda44ac88d73e9bdc7bf98f43eb8b8258bab4d
Ace360Degree-CC <166904430+Ace360Degree-CC@users.noreply.github.com>
2026-05-13 12:13:32 +0530

    Change DB host to 'localhost' in db.js
    Updated database connection settings to use 'localhost' instead of an IP address.

--- app/api/db.js
+++ app/api/db.js
@@ -1,16 +1,9 @@
-const mysql = require('mysql2/promise');
-
-// Create a connection pool
 export const pool = mysql.createPool({
-host: '66.116.237.145', // Usually 'localhost' works on cPanel
-user: 'kuwalsan_kuwalsanam',
-password: 'KSA@admin1',
-database: 'kuwalsan_maindb', // Must match the backend!
-  // host: '65.254.81.135',
-  // user: 'kuwalsanamarchit_kuwal_mainuser',
-  // password: `CZrK^OtQ$33^`,
-  // database: 'kuwalsanamarchit_maindb',
-  connectionLimit: 100,  // A more reasonable connection limit
+  host: 'localhost', // Use 'localhost' instead of the IP
+  user: 'kuwalsan_kuwalsanam',
+  password: 'KSA@admin1',
+  database: 'kuwalsan_maindb',
+  connectionLimit: 100,
   multipleStatements: true,  
   waitForConnections: true,
   queueLimit: 10000,
